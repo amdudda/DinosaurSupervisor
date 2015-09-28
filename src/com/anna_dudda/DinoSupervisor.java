8162eefacc423e6c961b3abd08682a8a61d671f7
amdudda <tk0654wm@go.minneapolis.edu>
2015-09-28 13:28:06 -0500

    added comments (oops)

--- src/com/anna_dudda/DinoSupervisor.java
+++ src/com/anna_dudda/DinoSupervisor.java
@@ -8,11 +8,14 @@ public class DinoSupervisor {
         Dinosaur velociraptor = new Dinosaur();
         Dinosaur apatosaurus = new Dinosaur();
 
+        // make t_rex roar, swipe, and stomp
         System.out.println("trex sez:");
         t_rex.roar(true);
         t_rex.stomp();
+        // make velocirapor roar, but not swipe.
         System.out.println("velociraptor sez:");
         velociraptor.roar(false);
+        // make apatosaurus roar, but not swipe.
         System.out.println("Apatosaurus sez:");
         apatosaurus.roar(false);
     }
